f8bf8fdfaf7cf65901a46845c620723323ff4569
akshithasrao <149546637+akshithasrao@users.noreply.github.com>
2024-06-13 21:42:41 -0500

    first commit

---
+++ creds.py
@@ -0,0 +1,6 @@
+# You need to give your credentials here
+class Creds:
+    connectionstring = 'ak3368db.cwphiuyl4eyu.us-east-1.rds.amazonaws.com'
+    username = 'admin'
+    passwd = 'as3368db'
+    dataBase = 'ak3368db'
